6509e68f5a5484d175564b96f49ba39b35dde08d
ekourtakis <emmanuel.kourtakis@temple.edu>
2025-09-17 15:42:43 -0400

    Add BackendConnector script and update README.md

--- README.md
+++ README.md
@@ -1,17 +1,49 @@
 # TranslatAR
 
-Run `docker compose up -d` to start everything.
+## Set Up
 
-Go to http://localhost:5173/ to see the webpage.
+### Docker containers
 
-Go to http://localhost:8000/api/data or http://localhost:8000/api/db-hello to fetch data from backend
+1. Run `docker compose up -d` to start everything. Initially donwloading the images may take a while.
+2. Go to <http://localhost:5173> to see the webpage.
+3. Go to <http://localhost:8000/api/data> or <http://localhost:8000/api/db-hello> to fetch data from backend.
 
 Run `docker compose down` to shut it off.
 
+### Unity frontend
+
 1. Install [Unity Hub](https://docs.unity3d.com/hub/manual/InstallHub.html)
 2. Open *Projects* tab and click *New project*
-3. Select the `unity` directory
+3. Locate this repository in your file system and select the `unity` directory
 4. Download reccomended version of Unity from pop up (this may take a while)
 5. Open the project
 
-The project should look like a blank 3D area
+The project should look like a blank 3D area.
+
+## Demo
+
+### Running the demo
+
+To demonstrate the connection between the containerized backend and the Unity frontend, first ensure all [Set Up](#set-up) steps are completed.
+
+1. Press the "Play" button (`▶`) at the top center of the Unity editor window to start the scene
+2. A pop up with the head set simulator should appear. There should be text on the simulator screen reading: "This is a subtitle." Walk around with `W`, `A`, `S`, and `D` keys. Look around with the arrow keys.
+3. Open the Unity console by selecting `Window > General > Console`, or press `Ctrl` + `Shift` + `C`
+4. Observe the log. If everything has gone right, it will read:
+
+    ```log
+    Received from backend: {"message":"Hello from MongoDB!"}
+    Message from backend: Hello from MongoDB!
+    ```
+
+### Data flow demonstrated
+
+1. The Unity frontend sends a request to the backend API endpoint
+2. The backend retrieves data from the MongoDB database
+3. The backend responds to Unity with the data
+4. Unity receives the response and logs the message in the console
+
+### Demo clean up
+
+Press the "Stop" button (`⏹`) next to the "Play" button to stop the scene.
+Run `docker compose down` to shut off Docker containers.
---
+++ unity/Assets/Scripts/BackendConnector.cs
@@ -0,0 +1,89 @@
+using UnityEngine;
+using UnityEngine.Networking;
+using System.Collections;
+
+// A simple class to demonstrate connecting to backend
+[System.Serializable]
+public class BackendMessage
+{
+    public string message;
+}
+
+public class BackendConnector : MonoBehaviour
+{
+    private const string backendUrl = "http://localhost:8000/api/db-hello";
+
+    void Start()
+    {
+        StartCoroutine(GetDataFromBackend());
+    }
+
+    IEnumerator GetDataFromBackend()
+    {
+        using (UnityWebRequest webRequest = UnityWebRequest.Get(backendUrl))
+        {
+            yield return webRequest.SendWebRequest();
+
+            switch (webRequest.result)
+            {
+                case UnityWebRequest.Result.ConnectionError:
+                case UnityWebRequest.Result.DataProcessingError:
+                case UnityWebRequest.Result.ProtocolError:
+                    Debug.LogError("Error: " + webRequest.error);
+                    break;
+                case UnityWebRequest.Result.Success:
+                    string jsonResponse = webRequest.downloadHandler.text;
+                    Debug.Log("Received from backend: " + jsonResponse);
+
+                    BackendMessage message = JsonUtility.FromJson<BackendMessage>(jsonResponse);
+
+                    Debug.Log("Message from backend: " + message.message);
+                    break;
+            }
+        }
+    }
+}using UnityEngine;
+using UnityEngine.Networking;
+using System.Collections;
+
+// A simple class to demonstrate connecting to backend
+[System.Serializable]
+public class BackendMessage
+{
+    public string message;
+}
+
+public class BackendConnector : MonoBehaviour
+{
+    private const string backendUrl = "http://localhost:8000/api/db-hello";
+
+    void Start()
+    {
+        StartCoroutine(GetDataFromBackend());
+    }
+
+    IEnumerator GetDataFromBackend()
+    {
+        using (UnityWebRequest webRequest = UnityWebRequest.Get(backendUrl))
+        {
+            yield return webRequest.SendWebRequest();
+
+            switch (webRequest.result)
+            {
+                case UnityWebRequest.Result.ConnectionError:
+                case UnityWebRequest.Result.DataProcessingError:
+                case UnityWebRequest.Result.ProtocolError:
+                    Debug.LogError("Error: " + webRequest.error);
+                    break;
+                case UnityWebRequest.Result.Success:
+                    string jsonResponse = webRequest.downloadHandler.text;
+                    Debug.Log("Received from backend: " + jsonResponse);
+
+                    BackendMessage message = JsonUtility.FromJson<BackendMessage>(jsonResponse);
+
+                    Debug.Log("Message from backend: " + message.message);
+                    break;
+            }
+        }
+    }
+}
