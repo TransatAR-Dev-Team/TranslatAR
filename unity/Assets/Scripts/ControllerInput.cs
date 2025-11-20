31d7bdb89bf3c20dc83c42e12da68acca60bb7dd
NateScott28 <143621201+NateScott28@users.noreply.github.com>
2025-11-20 01:53:36 -0500

    Updated packages and added an update to Controller script

--- unity/Assets/Scripts/ControllerInput.cs
+++ unity/Assets/Scripts/ControllerInput.cs
@@ -11,6 +11,9 @@ public class MenuController : MonoBehaviour
     //Main update function. Checks for OVR input. If Y or B pressed, toggle menu
     void Update()
     {
+
+        OVRInput.Update();
+
         // Use the Y button (left controller) or B button (right controller) to toggle menu
         if (OVRInput.GetDown(OVRInput.Button.Two)) // Button.Two = Y on left, B on right
         {
