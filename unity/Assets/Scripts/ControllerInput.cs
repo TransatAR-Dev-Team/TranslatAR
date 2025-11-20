3928cadff35cacb7803d4350c0f82e7071c6bd16
NateScott28 <143621201+NateScott28@users.noreply.github.com>
2025-11-20 01:10:41 -0500

    Create controller input script and SettingsManager object

---
+++ unity/Assets/Scripts/ControllerInput.cs
@@ -0,0 +1,27 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class MenuController : MonoBehaviour
+{
+    public GameObject settingsMenu; // Assign your settings menu Canvas in Inspector
+    private bool isMenuOpen = false;
+
+
+    //Main update function. Checks for OVR input. If Y or B pressed, toggle menu
+    void Update()
+    {
+        // Use the Y button (left controller) or B button (right controller) to toggle menu
+        if (OVRInput.GetDown(OVRInput.Button.Two)) // Button.Two = Y on left, B on right
+        {
+            ToggleMenu();
+        }
+    }
+
+    //Toggles menu active status
+    void ToggleMenu()
+    {
+        isMenuOpen = !isMenuOpen;
+        settingsMenu.SetActive(isMenuOpen);
+    }
+}
