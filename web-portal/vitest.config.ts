9e6254826324242e0be675feba80b550deb4d7a8
ekourtakis <emmanuel.kourtakis@temple.edu>
2025-12-06 13:05:04 -0500

    Exclude config files from test coverage reports

--- web-portal/vitest.config.ts
+++ web-portal/vitest.config.ts
@@ -1,6 +1,7 @@
 /// <reference types="vitest" />
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { configDefaults } from "vitest/config";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -11,8 +12,19 @@ export default defineConfig({
     setupFiles: "./src/test-setup.ts",
     coverage: {
       provider: "v8",
-      reporter: ["text", "json", "html"],
+      // json-summary is required for the coverage dashboard
+      reporter: ["text", "json", "html", "json-summary"],
       reportsDirectory: "./coverage",
+      exclude: [
+        ...configDefaults.exclude,
+        "postcss.config.js",
+        "tailwind.config.js",
+        "eslint.config.js",
+        "vite.config.ts",
+        "vitest.config.ts",
+        "src/main.tsx", // Entry point is usually excluded from unit tests
+        "src/vite-env.d.ts",
+      ],
     },
   },
 });
